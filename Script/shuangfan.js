564cff1ee48a16f3e4b9b65e209da6fb68ff16ba
JACKMR66 <150568012+JACKMR66@users.noreply.github.com>
2023-11-21 23:30:48 +0800

    Create shuangfan.js

---
+++ Script/shuangfan.js
@@ -0,0 +1,6 @@
+[rewrite_local]
+^https?:\/\/sadaslvcxjiaa\.vip\/front\/media\/(getMediaById|listShortVideoRandom) url script-request-header https://raw.githubusercontent.com/JACKMR66/MR/main/Script/crack.js
+^https?:\/\/sadaslvcxjiaa\.vip\/front\/(system\/bannersetting\/userGetBannerSet|cluser\/c\/user\/info\/get) url script-response-body https://raw.githubusercontent.com/JACKMR66/MR/main/Script/pure.js
+
+[mitm]
+hostname = sadaslvcxjiaa.vip
