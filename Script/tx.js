243c5d6fd0dd72461f7ff09b5dca2bb6b3a1d6ea
JACKMR66 <150568012+JACKMR66@users.noreply.github.com>
2023-11-22 00:10:48 +0800

    Create tx.js

---
+++ Script/tx.js
@@ -0,0 +1,4 @@
+^https?:\/\/tx.*\.com\/h5\/(user\/info|movie\/(block|search)|system\/info)$ url script-response-body https://raw.githubusercontent.com/JACKMR66/MR/main/Script/tangxin.js
+^https?:\/\/tx.*\.com\/h5\/movie\/detail$ url script-request-body https://raw.githubusercontent.com/JACKMR66/MR/main/Script/tangxin.js
+
+hostname = txh*.com, txk*.com, tx*.com
